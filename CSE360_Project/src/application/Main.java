693cabb9c194627c5dd0ad31386db4f24624dc7b
wyattnail <wyattnail@localhost>
2024-11-19 14:41:05 -0700

    Adding in the SQLite database as a .db file. This is done through DatabaseSetup and DatabaseConnection. Adjusted the main.java so it starts the connection before loading the login page.

--- CSE360_Project/src/application/Main.java
+++ CSE360_Project/src/application/Main.java
@@ -1,7 +1,18 @@
 package application;
-	
+
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
+    	
+    	Connection connection = DatabaseConnection.getConnection();
+    	if (connection != null) {
+    		System.out.println("Database connection successful");    		
+    	} else {
+    		System.out.println("Failed to connect to the database");
+    	}
+    	
+    	
         LoginPage.main(args); // Starts with the login page 
     }
 }
--- CSE360_Project/src/module-info.java
+++ CSE360_Project/src/module-info.java
@@ -2,6 +2,7 @@ module CSE360_HW1 {
 	requires javafx.controls;
 	requires javafx.base;
 	requires javafx.graphics;
+	requires java.sql;
 	 
 	opens application to javafx.graphics, javafx.fxml;
 }
